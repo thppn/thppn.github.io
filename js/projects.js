ea2fba952c6599c881b85144598272ff033e4e17
thppn <thppn@hotmail.com>
2025-07-31 19:59:07 +0300

    projects page updated

--- js/projects.js
+++ js/projects.js
@@ -4,15 +4,39 @@ fetch(`data/projects.json?nocache=${Date.now()}`)
     const container = document.getElementById('projects');
     data.forEach(project => {
         const card = document.createElement('div');
-        card.className = 'project-card'
+        card.className = "project-container"
         card.innerHTML = `
-			<h3 class="project-title">${project.name}</h3>
-			<p class="project-desc">${project.description}</p>
-			<div class="project-links">
-			<a target="_blank" rel="noopener noreferrer" href="${project.demo}">Live Demo</a>
-			<a target="_blank" rel="noopener noreferrer" href="${project.repo}">Source Code</a>
-			</div>
-        `;
+        <div class="project-image">
+            <img src="${project.img}"></img>
+        </div>
+        <div class="lang">
+            <h4>Languages</h4>
+            <ul>
+                <li>${project.lang[0]}</li>
+                <li>${project.lang[1]}</li>
+            </ul>
+        </div>
+        <div class="year">
+            <h4>Year</h4>
+                <span>${project.year}</span>
+            </div>
+        <div class="tech">
+            <h4>Tech</h4>
+            <ul>
+                <li>${project.tags[0]}</li>
+                <li>${project.tags[1]}</li>
+                <li>${project.tags[2]}</li>
+            </ul>
+        </div>
+        <div class="project-name">
+            ${project.name}
+        </div>`
+        card.innerHTML += `
+        <div class="project-links">
+            <a target="_blank" rel="noopener noreferrer" href="${project.demo}">Demo</a>
+            <a target="_blank" rel="noopener noreferrer" href="${project.repo}">Code</a>
+        </div>`
+
         container.appendChild(card);
     });
     });
