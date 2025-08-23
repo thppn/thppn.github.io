849537bb8892bc883e138c2c2215ac566a9fcad1
thppn <thppn@hotmail.com>
2025-08-23 14:53:42 +0300

    preloader added

---
+++ js/preloader.js
@@ -0,0 +1,23 @@
+// script.js
+const preloader = document.getElementById("preloader");
+const content = document.getElementById("content");
+
+const minTime = 2000; // 2s minimum
+const start = Date.now();
+
+window.addEventListener("load", function() {
+  const elapsed = Date.now() - start;
+
+  function showPage() {
+    preloader.classList.add("hidden");
+    setTimeout(() => {
+      content.classList.add("visible");
+    }, 800); // wait until fade-out finishes
+  }
+
+  if (elapsed < minTime) {
+    setTimeout(showPage, minTime - elapsed);
+  } else {
+    showPage();
+  }
+});
